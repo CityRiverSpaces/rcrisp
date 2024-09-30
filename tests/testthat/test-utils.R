9d6fef5bb6c8e4c2899e76b27e6b70374c20a406
Claudiu Forgaci <claudiuforgaci@gmail.com>
2024-09-30 21:44:30 +0200

    Update namespaces in UTM tests

--- tests/testthat/test-utils.R
+++ tests/testthat/test-utils.R
@@ -1,19 +1,21 @@
 test_that("correct UTM zone is returend in the southern hemisphere", {
   city_name <- "Chennai, India"
-  utm_from_location <- osmdata_as_sf("place", "city",
-                                     getbb(city_name))$osm_polygons |>
-    st_geometry() |>
-    st_as_sf() |>
-    get_utm_zone_epsg()
+  utm_from_location <-
+    CRiSp::osmdata_as_sf("place", "city",
+                         osmdata::getbb(city_name))$osm_polygons |>
+    sf::st_geometry() |>
+    sf::st_as_sf() |>
+    CRiSp::get_utm_zone_epsg()
   expect_equal(utm_from_location, 32644)
 })
 
 test_that("correct UTM zone is returend in the northern hemisphere", {
   city_name <- "Rejkjavik, Iceland"
-  utm_from_location <- osmdata_as_sf("place", "city",
-                                     getbb(city_name))$osm_polygons |>
-    st_geometry() |>
-    st_as_sf() |>
-    get_utm_zone_epsg()
+  utm_from_location <-
+    CRiSp::osmdata_as_sf("place", "city",
+                         osmdata::getbb(city_name))$osm_polygons |>
+    sf::st_geometry() |>
+    sf::st_as_sf() |>
+    CRiSp::get_utm_zone_epsg()
   expect_equal(utm_from_location, 32627)
 })
