4e39d9c32caa911e44468c2af9a1e160eacadffd
Claudiu Forgaci <claudiuforgaci@gmail.com>
2024-10-07 17:58:14 +0200

    Drop test for removed osm_bb() function

--- tests/testthat/test-osm_bb.R
+++
@@ -1,8 +0,0 @@
-# test_that("OSM bounding box is correctly retreived from Overpass API", {
-#   skip_on_ci()
-#
-#   bb <- CRiSp::osm_bb("Bucharest")
-#   expected <- matrix(c(25.966674, 44.334247, 26.225577, 44.541396),
-#                      ncol = 2, dimnames = list(c("x", "y"), c("min", "max")))
-#   expect_equal(bb, expected)
-# })
