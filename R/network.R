ff0515d41990a9e9a67e328839b57fcf7763fe58
Francesco Nattino <f.nattino@esciencecenter.nl>
2024-11-11 13:28:04 +0100

    add docstring

--- R/network.R
+++ R/network.R
@@ -1,6 +1,14 @@
-as_network <- function(edges, crs = NULL, flatten = FALSE, clean = FALSE) {
+#' Create a network from a line strings
+#'
+#' @param edges A data frame with the network edges
+#' @param flatten Whether all intersections between edges should be
+#'                converted to nodes
+#' @param clean Whether to run general cleaning tasks on the generated network
+#'
+#' @return A network object
+#' @export
+as_network <- function(edges, flatten = FALSE, clean = FALSE) {
   network <- sfnetworks::as_sfnetwork(edges, directed = FALSE)
-  if (!is.null(crs)) network <- sf::st_transform(network, crs)
   if (flatten) network <- flatten_network(network)
   if (clean) network <- clean_network(network)
   return(network)
