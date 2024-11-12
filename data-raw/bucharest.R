935d266fd8ddc826a15b17ea179cb0266da1fd75
Claudiu Forgaci <claudiuforgaci@gmail.com>
2024-11-12 14:52:05 +0100

    Fix encoding for all layers

--- data-raw/bucharest.R
+++ data-raw/bucharest.R
@@ -13,9 +13,11 @@ bbox_buffer <- 2000
 bucharest <- get_osmdata(city_name, river_name,
                          crs = epsg_code, buffer = bbox_buffer)
 
-# Fix encoding issue in the WKT string
-st_crs(bucharest$boundary)$wkt <- gsub("°|º", "\\\u00b0",
-                                       st_crs(bucharest$boundary)$wkt)
+# Fix encoding issue in the WKT string of city boundary
+lapply(names(bucharest), function(layer) {
+  sf::st_crs(bucharest[[layer]])$wkt <- gsub("°|º", "\\\u00b0",
+                                            sf::st_crs(bucharest[[layer]])$wkt)
+})
 
 # Save as package data
 usethis::use_data(bucharest, overwrite = TRUE)
