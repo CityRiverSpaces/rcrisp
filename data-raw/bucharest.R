e3f976b2b560d8cd46293e04b83816d36815b604
Claudiu Forgaci <claudiuforgaci@gmail.com>
2024-11-12 14:52:22 +0100

    Remove library() calls from data generation script

--- data-raw/bucharest.R
+++ data-raw/bucharest.R
@@ -1,8 +1,3 @@
-library(CRiSp)
-library(osmdata)
-library(dplyr)
-library(sf)
-
 # Set the parameters
 city_name <- "Bucharest"
 river_name <- "Dâmbovița"
@@ -10,8 +5,8 @@ epsg_code <- 32635
 bbox_buffer <- 2000
 
 # Fetch the data
-bucharest <- get_osmdata(city_name, river_name,
-                         crs = epsg_code, buffer = bbox_buffer)
+bucharest <- CRiSp::get_osmdata(city_name, river_name,
+                                crs = epsg_code, buffer = bbox_buffer)
 
 # Fix encoding issue in the WKT string of city boundary
 lapply(names(bucharest), function(layer) {
