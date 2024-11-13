eae8b978e22a4bb9c25d1f5c41d6fd2f50493365
Francesco Nattino <f.nattino@esciencecenter.nl>
2024-11-13 14:47:45 +0100

    refactoring batch for network and corridor delineation

--- R/delineate.R
+++ R/delineate.R
@@ -2,30 +2,52 @@
 #'
 #' @param city_name A place name as a string
 #' @param river_name A river name as a string
-#' @param crs The projected coordinate reference system (CRS) to use (default
-#'            to the suitable Universal Transverse Mercator (UTM) CRS)
+#' @param crs The projected Coordinate Reference System (CRS) to use. If not
+#'   provided, the suitable Universal Transverse Mercator (UTM) CRS is selected
+#' @param bbox_buffer Add a buffer region to the city boundary to avoid edge
+#'   effects close to its limits
+#' @param initial_method The method employed to define the initial river
+#'   corridor geometry. See [initial_corridor()] for the available methods
+#' @param capping_method The method employed to connect the corridor edge end
+#'   points (i.e. to "cap" the corridor). See [cap_corridor()] for
+#'   the available methods
 #' @param segments Whether to carry out the corridor segmentation
 #' @param riverspace Whether to carry out the riverspace delineation
 #'
 #' @return A simple feature geometry
 #' @export
-delineate_corridor <- function(city_name, river_name, crs = NULL,
-                               segments = FALSE, riverspace = FALSE) {
+delineate_corridor <- function(
+  city_name, river_name, crs = NULL, bbox_buffer = NULL,
+  initial_method = "buffer", capping_method = "direct", segments = FALSE,
+  riverspace = FALSE
+) {
+  # Retrieve all relevant OSM datasets using the extended bounding box
+  osm_data <- get_osmdata(city_name, river_name, crs, bbox_buffer)
 
-  osmdata <- CRiSp::get_osmdata(city_name, river_name, crs)
+  # Define the area of interest
+  bbox <- sf::st_transform(osm_data$bb, sf::st_crs(osm_data$boundary))
 
-  ...
+  # Setup the combined street and rail network for the delineation
+  network_edges <- dplyr::bind_rows(osm_data$streets, osm_data$railways)
+  network <- as_network(network_edges)
 
-  corridor <- CRiSp::corridor()
+  # Run the corridor delineation on the spatial network
+  corridor <- corridor(
+    network, osm_data$river_centerline, osm_data$river_surface, bbox,
+    initial_method, capping_method
+  )
+
   if (segments) delineate_segments()
   if (riverspace) delineate_riverspace()
   return(corridor)
 }
 
+#' @export
 delineate_segments <- function() {
   stop("Segmentation not yet implemented.")
 }
 
+#' @export
 delineate_riverspace <- function() {
   stop("Riverspace delineation not yet implemented.")
 }
