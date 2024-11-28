55cf64372e57b14c917b90365e47e881c134918f
Meiert Grootes <mwgrootes@Meierts-MacBook-Pro.local>
2024-11-28 07:04:24 +0100

    fixed linting issues

--- tests/testthat/test-valley.R
+++ tests/testthat/test-valley.R
@@ -4,22 +4,40 @@ test_that("STAC asset urls are correctly retrieved", {
   ep <- "https://earth-search.aws.element84.com/v1"
   col <- "cop-dem-glo-30"
 
-  asset_urls_retrieved <- get_stac_asset_urls(bb, endpoint = ep, collection = col)
+  asset_urls_retrieved <- get_stac_asset_urls(
+                                              bb, endpoint = ep,
+                                              collection = col)
   asset_urls_retrieved_default <- get_stac_asset_urls(bb)
 
-  expected_asset_urls <- c("s3://copernicus-dem-30m/Copernicus_DSM_COG_10_N44_00_E026_00_DEM/Copernicus_DSM_COG_10_N44_00_E026_00_DEM.tif",
-  "s3://copernicus-dem-30m/Copernicus_DSM_COG_10_N44_00_E025_00_DEM/Copernicus_DSM_COG_10_N44_00_E025_00_DEM.tif")
+  expasset1 <- paste0(
+                      "s3://copernicus-dem-30m/",
+                      "Copernicus_DSM_COG_10_N44_00_E026_00_DEM/",
+                      "Copernicus_DSM_COG_10_N44_00_E026_00_DEM.tif")
+  expasset2 <- paste0(
+                      "s3://copernicus-dem-30m/",
+                      "Copernicus_DSM_COG_10_N44_00_E025_00_DEM/",
+                      "Copernicus_DSM_COG_10_N44_00_E025_00_DEM.tif")
+
+  expected_asset_urls <- c(expasset1, expasset2)
   expect_equal(expected_asset_urls, asset_urls_retrieved)
   expect_equal(expected_asset_urls, asset_urls_retrieved_default)
 
-  })
+})
 
 
 test_that("raster data are correctly retrieved and merged", {
   skip_on_ci()
   bb <- osmdata::getbb("Bucharest")
-  raster_paths <- c("s3://copernicus-dem-30m/Copernicus_DSM_COG_10_N44_00_E026_00_DEM/Copernicus_DSM_COG_10_N44_00_E026_00_DEM.tif",
-  "s3://copernicus-dem-30m/Copernicus_DSM_COG_10_N44_00_E025_00_DEM/Copernicus_DSM_COG_10_N44_00_E025_00_DEM.tif")
+  rp1 <- paste0(
+                "s3://copernicus-dem-30m/",
+                "Copernicus_DSM_COG_10_N44_00_E026_00_DEM/",
+                "Copernicus_DSM_COG_10_N44_00_E026_00_DEM.tif")
+  rp2 <- paste0(
+                "s3://copernicus-dem-30m/",
+                "Copernicus_DSM_COG_10_N44_00_E025_00_DEM/",
+                "Copernicus_DSM_COG_10_N44_00_E025_00_DEM.tif")
+
+  raster_paths <- c(rp1, rp2)
 
   dem <- load_raster(bb, raster_paths)
   expected_dem <- terra::rast("path_to_example_data") #TODO add path and data
@@ -30,10 +48,13 @@ test_that("valley polygon is correctly constructed", {
   skip_on_ci()
   dem <- terra::rast("path_to_example_demd") #TODO add path and data
   river_geoms <- st_read("path_to_example_river") #TODO add path and data
-  river <- river_geoms[1,]  # select waterbody extent
-  crs <- 'epsg:32635'
+  river <- river_geoms[1, ]  # select waterbody extent
+  crs <- "epsg:32635"
+
   valley <- get_valley(dem, river, crs)
-  expected_valley <- st_read("path_to_example_valley") #TODO add example valley and path
+  expected_valley <- st_read("path_to_example_valley")
+  #TODO add example valley and path
+
   expect_equal(valley, expected_valley, tolerance = 1e-4)
 
 })
