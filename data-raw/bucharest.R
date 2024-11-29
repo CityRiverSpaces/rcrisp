793e4d6ded308c8e7f1a119ca2bf59d6196c03ad
Francesco Nattino <f.nattino@esciencecenter.nl>
2024-11-29 14:15:12 +0100

    add DEM to data

--- data-raw/bucharest.R
+++ data-raw/bucharest.R
@@ -4,17 +4,16 @@ river_name <- "Dâmbovița"
 epsg_code <- 32635
 bbox_buffer <- 2000
 
-# Fetch the data
-bucharest <- CRiSp::get_osmdata(city_name, river_name,
-                                crs = epsg_code, buffer = bbox_buffer)
+# Fix encoding issue for linter in the WKT string
+crs_wkt <- sf::st_crs(epsg_code)$wkt
+crs_wkt_fixed <- gsub("°|º", "\\\u00b0", crs_wkt)
 
-# Fix encoding issue in the WKT string of city boundary
-fix_wkt_encoding <- function(x) {
-  wkt <- sf::st_crs(x)$wkt
-  sf::st_crs(x)$wkt <- gsub("°|º", "\\\u00b0", wkt)
-  x
-}
-bucharest <- lapply(bucharest, fix_wkt_encoding)
+# Fetch the OSM data
+bucharest <- CRiSp::get_osmdata(city_name, river_name,
+                                crs = crs_wkt_fixed, buffer = bbox_buffer)
+# Add the DEM
+dem <- CRiSp::get_dem(bucharest$bb)
+bucharest <- c(bucharest, dem = reproject(dem, crs_wkt_fixed))
 
 # Save as package data
 usethis::use_data(bucharest, overwrite = TRUE)
