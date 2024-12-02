2bfd1fd35d3c4775e1cefa89e2c604b349f4a5bc
Francesco Nattino <f.nattino@esciencecenter.nl>
2024-12-02 13:50:32 +0100

    update tests and vignettes

--- tests/testthat/test-corridor.R
+++ tests/testthat/test-corridor.R
@@ -1,20 +1,20 @@
 test_that("river_buffer parameters can be configured via initial_corridor", {
-  river <- bucharest$river_centerline
+  river <- bucharest_osm$river_centerline
   actual <- initial_corridor(river, buffer = 1)
   expected <- river_buffer(river, buffer = 1)
   expect_setequal(actual, expected)
 })
 
 test_that("River buffer properly implements a buffer function", {
-  river <- bucharest$river_centerline
+  river <- bucharest_osm$river_centerline
   actual <- river_buffer(river, buffer = 0.5)
   expected <- sf::st_buffer(river, 0.5)
   expect_setequal(actual, expected)
 })
 
 test_that("River buffer can trim to the region of interest", {
-  river <- bucharest$river_centerline
-  bbox <- sf::st_bbox(bucharest$boundary)
+  river <- bucharest_osm$river_centerline
+  bbox <- sf::st_bbox(bucharest_osm$boundary)
   actual <- river_buffer(river, buffer = 0.5, bbox = bbox)
   river_buffer <- sf::st_buffer(river, 0.5)
   overlap_matrix <- sf::st_overlaps(river_buffer, actual, sparse = FALSE)
@@ -59,8 +59,8 @@ test_that("Splitting an AoI by a more complex river still gives two regions", {
 })
 
 test_that("Splitting an AoI by a river works with real data", {
-  bbox <- sf::st_bbox(bucharest$boundary)
-  river <- bucharest$river_centerline
+  bbox <- sf::st_bbox(bucharest_osm$boundary)
+  river <- bucharest_osm$river_centerline
   aoi_split <- split_aoi(bbox, river)
   expect_equal(length(aoi_split), 2)
 })
--- tests/testthat/test-data.R
+++ tests/testthat/test-data.R
@@ -1,51 +1,51 @@
 test_that("Bucharest dataset includes all elements", {
-  expect_setequal(names(bucharest),
+  expect_setequal(names(bucharest_osm),
                   c("bb", "boundary", "river_centerline", "river_surface",
                     "streets", "railways"))
 })
 
 test_that("Bounding box has correct type", {
-  bb <- bucharest$bb
+  bb <- bucharest_osm$bb
   expect_true(inherits(bb, "bbox"))
 })
 
 test_that("Only the bounding box has a geographic CRS", {
-  is_longlat <- sapply(bucharest, sf::st_is_longlat)
+  is_longlat <- sapply(bucharest_osm, sf::st_is_longlat)
   expect_true(is_longlat["bb"])
-  others <- is_longlat[names(bucharest) != "bb"]
+  others <- is_longlat[names(bucharest_osm) != "bb"]
   expect_true(!any(others))
 })
 
 test_that("City boundary has one multipolygon geometry", {
-  boundary <- bucharest$boundary
+  boundary <- bucharest_osm$boundary
   expect_equal(length(boundary), 1)
   expect_true(sf::st_is(boundary, "MULTIPOLYGON"))
 })
 
 test_that("River center line has one multilinestring geometry", {
-  river_centerline <- bucharest$river_centerline
+  river_centerline <- bucharest_osm$river_centerline
   expect_equal(length(river_centerline), 1)
   expect_true(sf::st_is(river_centerline, "MULTILINESTRING"))
 })
 
 test_that("River surface has one multipolygon geometry", {
-  river_surface <- bucharest$river_surface
+  river_surface <- bucharest_osm$river_surface
   expect_equal(length(river_surface), 1)
   expect_true(sf::st_is(river_surface, "MULTIPOLYGON"))
 })
 
 test_that("Streets are all linestrings", {
-  expect_true(all(sf::st_is(bucharest$streets, "LINESTRING")))
+  expect_true(all(sf::st_is(bucharest_osm$streets, "LINESTRING")))
 })
 
 test_that("Streets have type column", {
-  expect_equal(colnames(bucharest$streets), c("type", "geometry"))
+  expect_equal(colnames(bucharest_osm$streets), c("type", "geometry"))
 })
 
 test_that("Railways are all linestrings", {
-  expect_true(all(sf::st_is(bucharest$railways, "LINESTRING")))
+  expect_true(all(sf::st_is(bucharest_osm$railways, "LINESTRING")))
 })
 
 test_that("Railways have type column", {
-  expect_equal(colnames(bucharest$railways), c("type", "geometry"))
+  expect_equal(colnames(bucharest_osm$railways), c("type", "geometry"))
 })
