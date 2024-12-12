49f0493b33b591fc019afb064a9530935aa22912
Claudiu Forgaci <claudiuforgaci@gmail.com>
2024-12-12 21:31:03 +0100

    Make repeated objects from tests global

--- tests/testthat/test-valley.R
+++ tests/testthat/test-valley.R
@@ -1,3 +1,4 @@
+bb <- bucharest_osm$bb
 asset_urls <- c(paste0("s3://copernicus-dem-30m/",
                        "Copernicus_DSM_COG_10_N44_00_E026_00_DEM/",
                        "Copernicus_DSM_COG_10_N44_00_E026_00_DEM.tif"),
@@ -6,7 +7,6 @@ asset_urls <- c(paste0("s3://copernicus-dem-30m/",
                        "Copernicus_DSM_COG_10_N44_00_E025_00_DEM.tif"))
 
 test_that("STAC asset urls are correctly retrieved", {
-  bb <- bucharest_osm$bb
   ep <- "https://earth-search.aws.element84.com/v1"
   col <- "cop-dem-glo-30"
 
@@ -25,14 +25,11 @@ test_that("STAC asset urls are correctly retrieved", {
 })
 
 test_that("raster data are correctly retrieved and merged", {
-  bb <- bucharest_osm$bb
-  raster_paths <- asset_urls
-
   if (Sys.getenv("CI") == "true") {
     load_raster <- mockery::mock("load_raster", terra::unwrap(bucharest_dem))
   }
 
-  dem <- load_raster(bb, raster_paths) |> CRiSp::reproject(32635)
+  dem <- load_raster(bb, asset_urls) |> CRiSp::reproject(32635)
   expected_dem <- terra::unwrap(bucharest_dem)
   expect_equal(terra::values(dem), terra::values(expected_dem))
   expect_true(all.equal(terra::ext(dem), terra::ext(expected_dem),
