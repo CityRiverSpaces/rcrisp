79e6221f0b44ba5e552e398ab447129f06b47345
Claudiu Forgaci <claudiuforgaci@gmail.com>
2024-12-17 15:33:06 +0100

    Delete unnecessary setup.R script from tests

--- tests/testthat/setup.R
+++
@@ -1,5 +0,0 @@
-if (Sys.getenv("NOT_CRAN") != "true") {
-  dir.create("fixtures", showWarnings = FALSE)
-  file.copy("tests/testthat/fixtures/expected_valley.gpkg",
-            "fixtures/expected_valley.gpkg")
-}
