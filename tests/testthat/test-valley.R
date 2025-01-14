1a1b463cc6b45f4ce90bb17bbcf49a33ef844d16
Francesco Nattino <f.nattino@esciencecenter.nl>
2025-01-14 14:12:55 +0100

    add and improve tests

--- tests/testthat/test-valley.R
+++ tests/testthat/test-valley.R
@@ -19,7 +19,31 @@ test_that("STAC asset urls are correctly retrieved", {
   expect_equal(expected_asset_urls, asset_urls_retrieved)
 })
 
-test_that("raster data are correctly retrieved and merged", {
+test_that("load_raster correctly retrieve and merge local data", {
+
+  write_local_raster <- function(fname, xmin, xmax, ymin, ymax) {
+    rast <- terra::rast(xmin = xmin, xmax = xmax, ymin = ymin, ymax = ymax,
+                        res = 1, vals = 1, crs = "EPSG:4326")
+    terra::writeRaster(rast, fname)
+  }
+
+  bbox <- sf::st_bbox(c(xmin = 1, xmax = 4, ymin = 1, ymax = 7),
+                      crs = "EPSG:4326")
+  # create local rasters with adjacent bboxes
+  withr::with_file(list("r1.tif" = write_local_raster("r1.tif", 1, 4, 1, 4),
+                        "r2.tif" = write_local_raster("r2.tif", 1, 4, 4, 7)), {
+      rast <- load_raster(bbox, c("r1.tif", "r2.tif"))
+      # all values should be 1
+      expect_true(all(terra::values(rast) == 1))
+      # 2 rasters with 3x3 pixels -> 18 pixels in total
+      expect_length(terra::values(rast), 18)
+      # expect_equal on the two terra::ext objects somehow fails
+      expect_true(terra::ext(rast) == terra::ext(bbox))
+    }
+  )
+})
+
+test_that("load_raster correctly retrieve and merge remote data", {
   skip_on_ci()
 
   dem <- load_raster(bb, asset_urls)
