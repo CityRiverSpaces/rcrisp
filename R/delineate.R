e2279a3408551052b6b2d6e653c1f28795f776d3
Claudiu Forgaci <claudiuforgaci@gmail.com>
2025-01-20 15:55:38 +0100

    Move riverspace delineation to delineate module

--- R/delineate.R
+++ R/delineate.R
@@ -53,10 +53,24 @@ delineate_corridor <- function(
   return(corridor)
 }
 
-#' Delinate the riverspace.
+#' Delineate riverspace surrounding a river
 #'
-#' @return A simple feature geometry
+#' @param occluders Geometry of occluders
+#' @param river River centreline or surface
+#' @param rayno Number of rays
+#' @param raylen Length of rays
+#'
+#' @return Polygon geometry with the riverspace
 #' @export
-delineate_riverspace <- function() {
-  stop("Riverspace delineation not yet implemented.")
+#'
+#' @examples
+#' get_riverspace(bucharest_osm$buildings, bucharest_osm$river_surface)
+delineate_riverspace <- function(occluders, river, density = 1/50,
+                                 rayno = 41, raylen = 100) {
+  vpoints <- visor::get_viewpoints(river, density = density)
+  isovists <- lapply(vpoints, \(vpoint) {
+    visor::get_isovist(occluders, vpoint, rayno, raylen)
+  })
+  sf::st_union(isovists)
 }
+
