35b2bc29c3c5422f68062dbdd9a675896c65c5c7
Claudiu Forgaci <claudiuforgaci@gmail.com>
2025-02-20 23:02:38 +0100

    Add riverspace delineation tests

--- tests/testthat/test-riverspace.R
+++ tests/testthat/test-riverspace.R
@@ -1,8 +1,18 @@
-test_that("River centerline is used with warning when no river polygon
-          is available", {
-  expect_equal(2 * 2, 4)
-})
+riverspace_actual <- delineate_riverspace(occluders = bucharest_osm$buildings,
+                                          river = bucharest_osm$river_surface)
 
 test_that("The riverspace of Dâmbovița within 100m is correctly returned", {
-  expect_equal(2 * 2, 4)
+  actual_surface <- sf::st_area(actual)
+  expected_surface <- round(7742422, -5)
+  expect_equal(round(actual_surface, -5), expected_surface)
+})
+
+test_that("The area of the riverspace of Dâmbovița is smaller than
+          an unoccluded buffer and larger than the water surface", {
+  actual_surface <- units::set_units(sf::st_area(actual), "m^2")
+  river_surface_buffer <- sf::st_buffer(bucharest_osm$river_surface, 100)
+  river_surface_buffer_area <- sf::st_area(river_surface_buffer)
+  river_surface_area <- sf::st_area(bucharest_osm$river_surface)
+  expect_lt(actual_surface, river_surface_buffer_area)
+  expect_gt(actual_surface, river_surface_area)
 })
