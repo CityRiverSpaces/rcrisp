b5c860956aa9221f3abb3c8b97df9866f942691f
Claudiu Forgaci <claudiuforgaci@gmail.com>
2025-03-04 22:57:02 +0100

    Fix units in riverspace test

--- tests/testthat/test-riverspace.R
+++ tests/testthat/test-riverspace.R
@@ -7,16 +7,16 @@ test_that("The riverspace of Dâmbovița within 100m is correctly returned", {
   actual_surface <- sf::st_area(riverspace_actual)
   # A tolerance of 100,000 m^2 is used to account for changes in input data
   # (buildings added or removed in OSM)
-  expected_surface <- 7742422
-  expect_lt(abs(expected_surface - actual_surface), 1e05)
+  expected_surface <- units::set_units(7742422, "m^2")
+  expect_lt(abs(expected_surface - actual_surface),
+            units::set_units(1e05, "m^2"))
 })
 
 test_that(
   "The area of the riverspace of Dâmbovița is smaller than
   an unoccluded buffer and larger than the water surface",
   {
-    actual_surface <-
-      units::set_units(sf::st_area(riverspace_actual), "m^2")
+    actual_surface <- sf::st_area(riverspace_actual)
     river_surface_buffer <-
       sf::st_buffer(bucharest_osm$river_surface, 100)
     river_surface_buffer_area <- sf::st_area(river_surface_buffer)
