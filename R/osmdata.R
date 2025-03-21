7b8b786fb6ac315721cbf29998a725779f826683
Claudiu Forgaci <claudiuforgaci@gmail.com>
2025-03-21 11:27:19 +0100

    Fix invalid buildings retrieved from OSM

--- R/osmdata.R
+++ R/osmdata.R
@@ -346,6 +346,8 @@ get_osm_buildings <- function(aoi, crs = NULL, force_download = FALSE) {
   buildings <- osmdata_as_sf("building", "", aoi,
                              force_download = force_download)
   buildings <- buildings$osm_polygons |>
+    # retreived buildings may contain invalid polygons, fix these
+    sf::st_make_valid() |>
     sf::st_filter(aoi, .predicate = sf::st_intersects) |>
     dplyr::filter(.data$building != "NULL") |>
     sf::st_geometry()
