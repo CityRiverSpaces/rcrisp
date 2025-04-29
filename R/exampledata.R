51e4351dbb5a4d111bed157368f150acc6f537ba
Claudiu Forgaci <claudiuforgaci@gmail.com>
2025-04-30 00:25:07 +0200

    Remove useless comments from exampledata module

--- R/exampledata.R
+++ R/exampledata.R
@@ -1,5 +1,4 @@
 get_osm_example_data <- function() {
-  # Load example gpkg data from URL
   # nolint start
   url_osm <- "https://data.4tu.nl/file/f5d5e118-b5bd-4dfb-987f-fe10d1b9b386/f519315e-b92d-4815-b924-3175bd2a7a61"
   # nolint end
@@ -7,7 +6,6 @@ get_osm_example_data <- function() {
 }
 
 get_dem_example_data <- function() {
-  # Load example DEM data from URL
   # nolint start
   url_dem <- "https://data.4tu.nl/file/f5d5e118-b5bd-4dfb-987f-fe10d1b9b386/9eeee7aa-6005-48d6-ad63-d11c479db88b"
   # nolint end
