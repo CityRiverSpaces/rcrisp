01bc25e61a8b1786918f8556d2a01d1c2b22cc35
Claudiu Forgaci <33600128+cforgaci@users.noreply.github.com>
2025-07-04 13:42:28 +0200

    Update R/zzz.R
    Co-authored-by: Francesco Nattino <49899980+fnattino@users.noreply.github.com>

--- R/zzz.R
+++ R/zzz.R
@@ -1,6 +1,4 @@
 .onAttach <- function(libname, pkgname) {
-  try({
-    msg <- check_cache()
-    if (!is.null(msg)) packageStartupMessage(msg)
-  }, silent = TRUE)
+  msg <- check_cache()
+  if (!is.null(msg)) packageStartupMessage(msg)
 }
