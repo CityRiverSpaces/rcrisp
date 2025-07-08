5bb6c568e1e1340664a1d0058f7afe75235dbf40
Claudiu Forgaci <claudiuforgaci@gmail.com>
2025-07-08 21:21:52 +0200

    Remove reference to standard G1.4a from zzz.R

--- R/zzz.R
+++ R/zzz.R
@@ -1,6 +1,3 @@
-#' Check the cache when the package is attached
-#'
-#' @srrstats {G1.4a} Internal function documented in standard Roxygen format.
 #' @noRd
 .onAttach <- function(libname, pkgname) {
   check_cache()
