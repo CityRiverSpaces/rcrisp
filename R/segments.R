426700c31902f4229a0ec9476b5457ff9446c6ad
Claudiu Forgaci <claudiuforgaci@gmail.com>
2025-07-15 11:22:48 +0200

    Fix assertion in delineate_segments

--- R/segments.R
+++ R/segments.R
@@ -28,9 +28,7 @@ delineate_segments <- function(corridor, network, river,
   # Check input
   checkmate::assert_class(corridor, "sfc_POLYGON")
   checkmate::assert_class(network, "sfnetwork")
-  checkmate::assert_true(
-    inherits(river, c("sfc_LINESTRING", "sfc_MULTILINESTRING"))
-  )
+  checkmate::assert_true(inherits(river, c("sf", "sfc")))
   checkmate::assert_numeric(angle_threshold, lower = 90, upper = 180)
 
   # Drop all attributes of river but its geometry
