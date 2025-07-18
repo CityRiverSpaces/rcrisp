442830080d8986f1e47986d90e73444dd9f26d55
Claudiu Forgaci <claudiuforgaci@gmail.com>
2025-07-18 17:52:42 +0200

    Update text stating compliance with G2.2

--- R/srr-stats-standards.R
+++ R/srr-stats-standards.R
@@ -12,9 +12,11 @@
 #'   certain type using the `checkmate` package. It is made explicit in the
 #'   descriptions of such parameters that they should be of the required length.
 #' @srrstatsTODO {G2.2} Throughout the package, input validation is performed
-#'   using the `checkmate` package. This ensures that parameters expected to be
-#'   univariate vectors are appropriately restricted, and multivariate input
-#'   (such as matrices, data frames, or lists) is prohibited.
+#'   using vector-specific assertions from the `checkmate` package, such as
+#'   `assert_character()`, `assert_numeric()`, or the more general
+#'   `assert_vector()`. This ensures that parameters expected to be univariate
+#'   vectors are appropriately restricted, and multivariate input (such as
+#'   matrices, data frames, or lists) is prohibited.
 #' @srrstatsTODO {G2.3} *For univariate character input:*
 #' @srrstatsTODO {G2.3a} *Use `match.arg()` or equivalent where applicable to only permit expected values.*
 #' @srrstatsTODO {G2.3b} *Either: use `tolower()` or equivalent to ensure input of character parameters is not case dependent; or explicitly document that parameters are strictly case-sensitive.*
