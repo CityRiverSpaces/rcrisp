cb360bfda9f9cd5f8c2bfe8b705ec6eb3067b936
Claudiu Forgaci <claudiuforgaci@gmail.com>
2025-07-21 16:42:30 +0200

    Apply SP4.0-4.2

--- R/exampledata.R
+++ R/exampledata.R
@@ -14,13 +14,16 @@
 #'   from a persistent URL. The code used to generate the example dataset is
 #'   publicly available, as described in the Roxygen comments.
 #'
-#' @return A list of sf objects containing the OSM data.
+#' @return A list of sf objects containing the OSM data as [`sf::sfc`]
+#'   objects.
 #' @importFrom utils download.file
 #' @importFrom stats setNames
 #' @export
 #'
 #' @examplesIf interactive()
 #' get_osm_example_data()
+#' @srrstats {SP4.0, SP4.0b, SP4.2} The function returns a list of
+#'   [`sf::sfc`] objects, explicitly documented as such.
 get_osm_example_data <- function() {
   url_osm <- "https://data.4tu.nl/file/f5d5e118-b5bd-4dfb-987f-fe10d1b9b386/f519315e-b92d-4815-b924-3175bd2a7a61"  # nolint
   temp_file <- tempfile(fileext = ".gpkg")
@@ -40,11 +43,13 @@ get_osm_example_data <- function() {
 #' in examples and tests. The code used to generate the example dataset is
 #' available at https://github.com/CityRiverSpaces/CRiSpExampleData.
 #'
-#' @return A SpatRaster object containing the DEM data.
+#' @return A [`terra::SpatRaster`] object containing the DEM data.
 #' @export
 #'
 #' @examplesIf interactive()
 #' get_dem_example_data()
+#' @srrstats {SP4.0, SP4.0b, SP4.2} The function returns a [`terra::SpatRaster`]
+#'   object, explicitly documented as such.
 get_dem_example_data <- function() {
   url_dem <- "https://data.4tu.nl/file/f5d5e118-b5bd-4dfb-987f-fe10d1b9b386/9eeee7aa-6005-48d6-ad63-d11c479db88b"  # nolint
   terra::rast(url_dem)
