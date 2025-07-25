2399fda4765412f06def06cae530998240bd5f9c
Claudiu Forgaci <claudiuforgaci@gmail.com>
2025-07-25 22:29:14 +0200

    Remove SP6.2 from TODO

--- R/srr-stats-standards.R
+++ R/srr-stats-standards.R
@@ -73,7 +73,6 @@
 #' @srrstatsTODO {SP6.1} *All functions which can be applied to both Cartesian and curvilinear data should be tested through application to both.*
 #' @srrstatsTODO {SP6.1a} *Functions which may yield inaccurate results when applied to data in one or the other forms (such as the preceding examples of centroids and buffers from ellipsoidal data) should test that results from inappropriate application of those functions are indeed less accurate.*
 #' @srrstatsTODO {SP6.1b} *Functions which yield accurate results regardless of whether input data are rectilinear or curvilinear should demonstrate equivalent accuracy in both cases, and should also demonstrate how equivalent results may be obtained through first explicitly transforming input data.*
-#' @srrstatsTODO {SP6.2} *Geographical Software should include tests with extreme geographical coordinates, minimally including extension to polar extremes of +/-90 degrees.*
 # nolint end
 #' @noRd
 NULL
