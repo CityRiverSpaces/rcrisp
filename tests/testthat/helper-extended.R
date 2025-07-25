e3f59780aa2c6e1c96f94a9aa197875e915d364f
Francesco Nattino <f.nattino@esciencecenter.nl>
2025-07-25 15:32:44 +0200

    fixing lint issue and testing if run-extended works

--- tests/testthat/helper-extended.R
+++ tests/testthat/helper-extended.R
@@ -1,4 +1,4 @@
 skip_if_not_extended <- function() {
   env_var <- as.logical(Sys.getenv("RCRISP_EXTENDED_TESTS", unset = "false"))
-  skip_if_not(!is.na(env_var) && env_var, "Skipping extended test")
+  testthat::skip_if_not(!is.na(env_var) && env_var, "Skipping extended test")
 }
