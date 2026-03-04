2104ab0ba209dcef5ec73dfd9199ca1c408e5915
Claudiu Forgaci <33600128+cforgaci@users.noreply.github.com>
2026-03-04 14:26:40 +0200

    Apply suggestions from code review
    Suggested by @fnattino

--- R/zzz.R
+++ R/zzz.R
@@ -1,6 +1,6 @@
 #' @noRd
 .onLoad <- function(libname, pkgname) {
-  if (is.null(getOption("rcrisp.run_checks"))) {
+  if (is.null(getOption("rcrisp.check_cache"))) {
     options(rcrisp.run_checks = FALSE)
   }
   invisible()
@@ -8,7 +8,7 @@
 
 #' @noRd
 .onAttach <- function(libname, pkgname) {
-  if (interactive() || getOption("rcrisp.run_checks")) {
+  if (interactive() || getOption("rcrisp.check_cache")) {
     check_cache()
   }
 }
