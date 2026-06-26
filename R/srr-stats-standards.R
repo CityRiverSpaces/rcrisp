122e866365a693cafe2fa00b7c1d76b60017cc12
Claudiu Forgaci <claudiuforgaci@gmail.com>
2026-06-26 08:30:29 +0200

    Fix typo in srrstats

--- R/srr-stats-standards.R
+++ R/srr-stats-standards.R
@@ -67,7 +67,7 @@ NULL
 #' @srrstatsNA {G1.6} As there are no alternative implementations, no
 #'   performance claims are made in this package.
 #' @srrstatsNA {G2.4d, G2.4e, G2.5} This package does not make use of factors.
-x#' @srrstatsNA {G2.9} This package does not perform type conversions or
+#' @srrstatsNA {G2.9} This package does not perform type conversions or
 #'   meta-data changes leading to information loss that would require issuing
 #'   diagnostic messages.
 #' @srrstatsNA {G2.11, G2.12} This package does not utilize list columns or
